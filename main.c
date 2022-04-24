b7c95244ac450f13ffe4a027bf4e33fee92792f8
dshirely <80853640+den1sssss@users.noreply.github.com>
2022-04-24 21:59:38 +0300

    Add files via upload
    24/04 =-) today was a good day ultrakek

---
+++ main.c
@@ -0,0 +1,12 @@
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <string.h>
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <signal.h>
+# include <errno.h>
+# include <fcntl.h>
+# include <term.h>
+# include <libft/libft.h>
+
